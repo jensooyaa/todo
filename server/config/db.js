20473dc3473e887d86deba9679db3b3d7344b1f7
huangxin0510 <huangxin0510@focuschina.com>
2026-03-10 18:44:26 +0800

    AIPRD-284 打包

--- server/config/db.js
+++ server/config/db.js
@@ -9,6 +9,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   timezone: '+08:00',
+  dateStrings: true,
   waitForConnections: true,
   connectionLimit: 10,
 });
